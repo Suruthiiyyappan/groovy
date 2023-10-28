6172f37e919f8a782a717fdb2e044c599a14deeb
Suruthi <96717233+Suruthiiyyappan@users.noreply.github.com>
2023-10-28 20:08:36 +0530

    Update mvnbuild.groovy

--- vars/mvnbuild.groovy
+++ vars/mvnbuild.groovy
@@ -1,21 +1,12 @@
 def call() {
     if (ENV == 'mule') {
         sh 'mvn clean package'
+        archiveJar()
     } else {
         echo 'not build'
     }
 }
 
-
-// def call() {
-//     if (ENV == 'mule') {
-//         sh 'mvn clean package'
-//         archiveJar()
-//     } else {
-//         echo 'not build'
-//     }
-// }
-
-// def archiveJar() {
-//     archiveArtifacts artifacts: '**/target/*.jar', allowEmptyArchive: true
-// }
+def archiveJar() {
+    archiveArtifacts artifacts: '**/target/*.jar', allowEmptyArchive: true
+}
