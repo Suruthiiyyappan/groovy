69b1e5a11eda3a07270e8badd70efaf3e54aef5f
Suruthi <96717233+Suruthiiyyappan@users.noreply.github.com>
2023-10-28 20:58:11 +0530

    Update qualitygate.groovy

--- vars/qualitygate.groovy
+++ vars/qualitygate.groovy
@@ -1,9 +1,9 @@
 def call() {
-    def qg = waitForQualityGate()
-    if (qg.status != 'OK') {
-        error "Quality gate not OK: ${qg.status}"
+    timeout(time: 1, unit: 'HOURS') { 
+        waitForQualityGate abortPipeline: true
+            def qg = waitForQualityGate()
+            if (qg.status != 'OK') {
+                error "Quality gate not OK: ${qg.status}"
+            }
     }
 }
-
-
-
