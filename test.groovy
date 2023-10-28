0bde21453c7535923323ca779d986406e5bbbc8d
Suruthi <96717233+Suruthiiyyappan@users.noreply.github.com>
2023-10-28 21:39:29 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -17,4 +17,13 @@ def sonar() {
     -Dsonar.projectName=ss"
     }
 
+def call() {
+    timeout(time: 1, unit: 'HOURS') { 
+        waitForQualityGate abortPipeline: true
+            def qg = waitForQualityGate()
+            if (qg.status != 'OK') {
+                error "Quality gate not OK: ${qg.status}"
+            }
+    }
+}
 return this
